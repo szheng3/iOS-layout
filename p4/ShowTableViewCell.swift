c49efa505d7d15d743cefe0066e6eaa7ecdcda7e
Shuai <szheng3@uci.edu>
2020-04-22 15:41:51 -0500

    ui enforced

--- p4/ShowTableViewCell.swift
+++ p4/ShowTableViewCell.swift
@@ -9,45 +9,48 @@ import PinLayout
 
 class ShowTableViewCell: UITableViewCell {
     static var reuseIdentifier = "ShowTableViewCell"
-    let back = UIView()
     var leftImage = UIImage(named: "nv") {
         willSet(newValue) {
             imageViewLS.image = newValue
         }
     };
-    lazy var imageViewLS: UIImageView = {
+    var uiLabel: UILabel = {
+        let label = UILabel();
+        label.adjustsFontSizeToFitWidth = true
+        return label;
+
+    }();
+
+    private lazy var imageViewLS: UIImageView = {
         let ui = UIImageView(image: self.leftImage);
         ui.contentMode = UIView.ContentMode.scaleAspectFit
         return ui;
 
     }()
 
+    var margin: CGFloat = 8{
+        didSet{
+            setUpUI()
+        }
+    };
 
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        back.backgroundColor = .blue
 
+        setUpUI()
 
-        contentView.flex.direction(.row).alignItems(.center).define { flex in
-            flex.addItem(imageViewLS).height(100).aspectRatio(1)
-        }
-
-//        self.textLabel?.textColor = .white
-//        self.textLabel?.font = .systemFont(ofSize: 14.0)
-//        self.textLabel?.numberOfLines = 2
-//        self.textLabel?.adjustsFontSizeToFitWidth = true
-//        self.textLabel?.minimumScaleFactor = 0.8
-//        self.detailTextLabel?.textColor = .lightGray
-//
-//        let accessoryView = UIImageView(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
-//        accessoryView.image = UIImage(named: "custom-ios-app-development1")
-//        self.accessoryView = accessoryView
-//
-//        self.backgroundColor = .clear
-//        self.separatorInset = .zero
     }
 
+    func setUpUI(){
+
+        contentView.flex.direction(.row).alignItems(.center).define { flex in
+            flex.addItem(imageViewLS).marginLeft(self.margin).height(100).aspectRatio(1).markDirty()
+            flex.addItem(uiLabel);
+
+        }
+
+    }
     override func layoutSubviews() {
         contentView.flex.layout(mode: .fitContainer)
 
@@ -64,6 +67,11 @@ class ShowTableViewCell: UITableViewCell {
 //        self.textLabel?.text = show.title
 //        self.detailTextLabel?.text = show.length
         self.leftImage = UIImage(named: show.image)
+        self.uiLabel.text = show.title
+//        self.margin=100;
+//        setNeedsLayout()
+
+
 
     }
 
